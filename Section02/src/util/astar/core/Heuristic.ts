b8ae7b222aed1a7835909a5e7745ad5be481f0f4
jkasun <janithisipathana@gmail.com>
2019-04-27 11:54:44 +0530

    Astar Algorithm For Path Finding

---
+++ Section02/src/util/astar/core/Heuristic.ts
@@ -0,0 +1,9 @@
+/**
+ * Calculate the manhatten distance.
+ * @param  {number} _dx           [ Horizontal change ]
+ * @param  {number} _dy           [ Vertical change ]
+ * @return {number}               [ The calculated distance ]
+ */
+export function getManhattenDistance(_dx: number, _dy: number): number {
+ return _dx + _dy;
+}
